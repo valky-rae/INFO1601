ba985daca6b4710c6a55695295dc061b589fc48f
RaeanneRamharrack <raeanne.ramharrack@my.uwi.edu>
2020-04-15 20:45:06 -0400

    router-link

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -7,11 +7,8 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret, faHome, faPencilAlt, faInfoCircle, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faUserSecret)
 library.add(faHome)
-library.add(faPencilAlt)
 library.add(faInfoCircle)
-library.add(faArrowAltCircleRight)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
