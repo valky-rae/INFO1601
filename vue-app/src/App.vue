861a01f7e369c0fbd4a73abbc1eea70687426c39
RaeanneRamharrack <raeanne.ramharrack@my.uwi.edu>
2020-04-22 03:04:43 -0400

    nav bar

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,63 +1,79 @@
 <template>
   <div id="app">
-    <FallDown class="nav">
-      <span>
-        <font-awesome-icon class="nav" icon="home" />
-        <router-link class="nav" to="/home"> Home</router-link>
-      </span>
-      <span>
-        <font-awesome-icon class="nav" icon="info-circle"/>
-        <router-link class="nav" to="/about"> About</router-link>
-      </span>
-    </FallDown>
-    <main id="page-wrap">
-      <h1> PAGE WRAPPER THINGY </h1>
-      <div class="lines">
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-      </div>
+    <div id="navbar">
+      <router-link class="rounded" to="/home"> Home </router-link>
+      <router-link class="rounded" to="/about"> About </router-link>
+    </div>
+    <main>
+      <!-- <div class="lines">
+        <div class="line"> </div>
+      </div> -->
       <router-view/>
     </main>
-    <!-- <img src="./assets/logo.png"> -->
+    <footer>
+      <span>
+        Copyright &copy; All Rights Reserved
+        <!-- <img id="Vlogo" src="./assets/marvel-logo.jpg" width="100" height="67"> -->
+      </span>
+    </footer>
   </div>
 </template>
 
 <script>
-import { FallDown } from 'vue-burger-menu'
 
 export default {
-  components: {
-    FallDown
-  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Poppins', sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.nav {
-  padding: 10px;
-  font-size: 25px;
-  font-weight: bold;
-  color: white;
+  color:white;
+  margin-top: 1%;
 }
 a {
-    font-weight: bold;
-    color: #2c3e50;
+    font-family: 'Bangers', cursive;
+    color:white;
     text-decoration:  none;
-    padding-top: 50px;
-    text-align: bottom;
+    padding-left: 20px;
+    padding-right: 16px;
   }
-svg{
-  padding-bottom: 100px;
+.rounded {
+  font-size: 30px;
+  color: white;
+  border-radius: 30px;
+  border: 3px solid white;
+  width: 150px;
+  height: 30px;
+  padding-top: 5px;
+  padding-bottom: 10px;
+}
+#navbar{
+  margin-right: 60px;
+  text-align: right;
+  text-shadow: 3px 3px black;
+}
+#navbar :first-child {
+  margin-right: 20px;
+}
+#navbar a:hover {
+  background-color: white;
+  color: red;
+  /* text-shadow: 3px 3px red; */
+}
+footer {
+  font-family: 'Bangers', cursive;
+  text-align: center;
+  color: white;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  padding-bottom: 15px;
+}
+#Vlogo{
+  margin-right: 60px;
+ float: right;
 }
 </style>
