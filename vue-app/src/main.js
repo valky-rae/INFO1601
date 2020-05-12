cd38db4b8656e08e72db5220ad187c8f68788e93
rajsadho <rajsadho@users.noreply.github.com>
2020-05-11 23:09:27 -0400

    Trying out bootstrap-vue

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -3,10 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHome, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 library.add(faHome)
 library.add(faInfoCircle)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
